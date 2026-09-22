11e5255dff23d119fbac612aa3135b582eab753e
medanprintonline-creator <medanprintonline@gmail.com>
2026-09-22 17:17:21 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,42 @@
 /* BandaAcehPrint Service Worker */
 const CACHE_NAME = 'bandaacehprint-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/app.js',
-  '/manifest.json'
+    './',
+    './index.html',
+    './css/style.css',
+    './js/app.js',
+    './manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  // Skip non-GET
-  if (event.request.method !== 'GET') return;
+    if (event.request.method !== 'GET') return;
 
-  // Skip WhatsApp, Google Maps, dll.
-  const url = new URL(event.request.url);
-  if (url.origin !== location.origin) return;
+    const url = new URL(event.request.url);
+    if (url.origin !== location.origin) return;
 
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(res => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(c => c.put(event.request, clone));
-        return res;
-      }).catch(() => caches.match('/index.html'));
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(cached => {
+            return cached || fetch(event.request).then(res => {
+                const clone = res.clone();
+                caches.open(CACHE_NAME).then(c => c.put(event.request, clone));
+                return res;
+            }).catch(() => caches.match('./index.html'));
+        })
+    );
 });
